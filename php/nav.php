bde48606c5c87cbe35e7c555d914795c72f6adfc
Jake Cortez <jcortez96@hotmail.com>
2014-08-27 16:43:39 -0600

    refactored nav to php format

--- php/nav.php
+++ php/nav.php
@@ -10,39 +10,39 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="home.html">Group-Out</a>
+              <a class="navbar-brand" href="index.php">Group-Out</a>
             </div>
         
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul class="nav navbar-nav">
                 <li class="dropdown">
-                  <a href="routes.html" class="dropdown-hover" data-hover="dropdown">Routes<span class="caret"></span></a>
+                  <a href="routes.php" class="dropdown-hover" data-hover="dropdown">Routes<span class="caret"></span></a>
                   <ul class="dropdown-menu dropdown-hover" data-hover="dropdown" role="menu">
-                    <li><a href="route-map.html">Route Map</a></li>
-                    <li><a href="my-routes.html">My Routes</a></li>
+                    <li><a href="route-map.php">Route Map</a></li>
+                    <li><a href="my-routes.php">My Routes</a></li>
                   </ul>
                 </li>
                 <li class="dropdown">
-                  <a href="events.html"class="dropdown-hover" data-hover="dropdown">Events<span class="caret"></span></a>
+                  <a href="events.php"class="dropdown-hover" data-hover="dropdown">Events<span class="caret"></span></a>
                   <ul class="dropdown-menu dropdown-hover" data-hover="dropdown" role="menu">
-                    <li><a href="my-events.html">My Events</a></li>
-                    <li><a href="group-events.html">Group Events</a></li>
+                    <li><a href="my-events.php">My Events</a></li>
+                    <li><a href="group-events.php">Group Events</a></li>
                   </ul>
                 </li>
                 <li class="dropdown">
-                  <a href="groups.html" class="dropdown-hover" data-hover="dropdown">Groups<span class="caret"></span></a>
+                  <a href="groups.php" class="dropdown-hover" data-hover="dropdown">Groups<span class="caret"></span></a>
                   <ul class="dropdown-menu dropdown-hover" data-hover="dropdown" role="menu">
-                    <li><a href="my-groups.html">My Groups</a></li>
-                    <li><a href="group-manage.html">Groups Management</a></li>
+                    <li><a href="my-groups.php">My Groups</a></li>
+                    <li><a href="group-manage.php">Groups Management</a></li>
                   </ul>
                 </li>
                 <li class="dropdown">
-                  <a href="community.html"class="dropdown-hover" data-hover="dropdown">Community<span class="caret"></span></a>
+                  <a href="community.php"class="dropdown-hover" data-hover="dropdown">Community<span class="caret"></span></a>
                   <ul class="dropdown-menu dropdown-hover" data-hover="dropdown" role="menu">
-                    <li><a href="people.html">Find People</a></li>
-                    <li><a href="forums.html">Forums</a></li>
-                    <li><a href="about.html">About Us</a></li>
+                    <li><a href="people.php">Find People</a></li>
+                    <li><a href="forums.php">Forums</a></li>
+                    <li><a href="about.php">About Us</a></li>
                   </ul>
                 </li>
               </ul>
