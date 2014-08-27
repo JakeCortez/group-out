095007808acfa1c0e6a5c9d68262ec4f8d4fadfd
Jake Cortez <jcortez96@hotmail.com>
2014-08-27 12:39:54 -0600

    added directory infrastructure and related files

---
+++ php/nav.php
@@ -0,0 +1,58 @@
+
+    <!-- Navbar: must be consistent amongst pages-->
+        <nav class="navbar navbar-default" role="navigation">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="home.html">Group-Out</a>
+            </div>
+        
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li class="dropdown">
+                  <a href="routes.html" class="dropdown-hover" data-hover="dropdown">Routes<span class="caret"></span></a>
+                  <ul class="dropdown-menu dropdown-hover" data-hover="dropdown" role="menu">
+                    <li><a href="route-map.html">Route Map</a></li>
+                    <li><a href="my-routes.html">My Routes</a></li>
+                  </ul>
+                </li>
+                <li class="dropdown">
+                  <a href="events.html"class="dropdown-hover" data-hover="dropdown">Events<span class="caret"></span></a>
+                  <ul class="dropdown-menu dropdown-hover" data-hover="dropdown" role="menu">
+                    <li><a href="my-events.html">My Events</a></li>
+                    <li><a href="group-events.html">Group Events</a></li>
+                  </ul>
+                </li>
+                <li class="dropdown">
+                  <a href="groups.html" class="dropdown-hover" data-hover="dropdown">Groups<span class="caret"></span></a>
+                  <ul class="dropdown-menu dropdown-hover" data-hover="dropdown" role="menu">
+                    <li><a href="my-groups.html">My Groups</a></li>
+                    <li><a href="group-manage.html">Groups Management</a></li>
+                  </ul>
+                </li>
+                <li class="dropdown">
+                  <a href="community.html"class="dropdown-hover" data-hover="dropdown">Community<span class="caret"></span></a>
+                  <ul class="dropdown-menu dropdown-hover" data-hover="dropdown" role="menu">
+                    <li><a href="people.html">Find People</a></li>
+                    <li><a href="forums.html">Forums</a></li>
+                    <li><a href="about.html">About Us</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+              </form>
+              <button type="button" class="btn btn-default navbar-btn">Sign in</button>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
