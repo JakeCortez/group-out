08910ddaeea38f9408d05a356a6e4b9cf7a7ed4b
Jake Cortez <jcortez96@hotmail.com>
2014-08-29 11:36:33 -0600

    added most of pages for website

---
+++ pages/index.php
@@ -0,0 +1,88 @@
+ <!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Group Out</title>
+   <!--custom css and javascript-->
+    <link href="../css/style.css" rel="stylesheet">
+    <script src="../js/group-out.js"></script>
+    <!-- Bootstrap -->
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="page-header">
+      <h1>group<strong>out</strong><small> lorem ipsum</small></h1>
+    </div>
+    <?php require_once('../php/nav.php');?>
+    <div class = "container-fluid">
+      <div class = "row">
+        <article class = "col-lg-12">
+          <!--carousel start-->
+          <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="../images/share_routes.jpg" data-slide-to="0" class="active"></li>
+              <li data-target="../images/meet_people.jpg" data-slide-to="1"></li>
+              <li data-target="../images/make_events.jpg" data-slide-to="2"></li>
+            </ol>
+          
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+              <div class="item active">
+                <img src="../images/share_routes.jpg" alt="...">
+                <div class="carousel-caption">
+                  <h3>Share your favorite routes</h3>
+                </div>
+              </div>
+              <div class="item">
+                <img src="../images/meet_people.jpg" alt="...">
+                <div class="carousel-caption">
+                  <h3>Meet new people</h3>
+                </div>
+              </div>
+                <div class="item">
+                <img src="../images/make_events.jpg" alt="...">
+                <div class="carousel-caption">
+                  <h3>Make an event for your friends to attend</h3>
+                </div>
+              </div>
+            </div>
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left"></span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right"></span>
+            </a>
+          </div>
+        </article>
+      </div>
+      <div class = "row">
+        <div class = "col-lg-12" id = "new-user-index">
+          <p>Make an account today with just your e-mail!</p>
+          <form>
+            <input type = "email" placeholder = "youremail@email.com" name = "new-email">
+            <input type = "password" placeholder = "password">
+            <input type = "password" placeholder = "confirm password">
+            <button class = "btn btn-default" type="submit">Sign Up!</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <?php require_once('../php/footer.php');?>
+    
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
