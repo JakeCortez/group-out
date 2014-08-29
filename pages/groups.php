4986bf162766282fdb09c433b5715463367a4dcb
Jake Cortez <jcortez96@hotmail.com>
2014-08-29 12:03:50 -0600

    added pages and started javascript page

---
+++ pages/groups.php
@@ -0,0 +1,40 @@
+ <!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Group Out</title>
+   <!--custom css and javascript-->
+    <link href="../css/style.css" rel="stylesheet">
+    <script src="../js/group-out.js"></script>
+    <!-- Bootstrap -->
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    
+    <?php require_once('../php/nav.php');?>
+    <div class = "container-fluid">
+      <article class = "col-lg-9">
+        
+      </article>
+    
+      <aside class = "col-lg-3">
+      <?php require_once('../php/sidebarplaceholder.php');?>
+      </aside>
+    </div>
+    <?php require_once('../php/footer.php');?>
+    
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
