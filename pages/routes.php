ee31911e5d564b485da35f6e8c2e88b268452ad8
Jake Cortez <jcortez96@hotmail.com>
2014-09-04 17:26:33 -0600

    finsinishing up routes page

--- pages/routes.php
+++ pages/routes.php
@@ -2,51 +2,214 @@
 <html>
 
 <?php require_once('../php/head.html');?>
+  
   <body>
-    
     <?php require_once('../php/nav.html');?>
     <div class = "container-fluid">
-        <section class = "col-lg-9">
-          <div class = "row">
-            <h3>find a route</h3>
+        <section class = "col-lg-9 col-md-9 col-sm-6">
+          <div class = "page_info">
+            <h2>find a route</h2>
             <h3><small>choose your activity and difficulty level</small></h3>
             <h5>activity:   <a>hike</a> | <a>run</a>    | <a>bike</a> | <a>all</a></h5>
             <h5>difficulty: <a>low</a>  | <a>medium</a> | <a>high</a> | <a>all</a></h5>
           </div>
-          <nav class = "row">
-            <ul class = "nav nav-tabs" role = "tablist">
-              <li>recent</li>
-              <li>popular</li>
-              <li>random</li>
-              <li>all</li>
+          <nav class = "navbar-default navbar" role = "navigation">
+          <div class = "route-nav">
+            <!-- Nav tabs -->
+            <ul class="nav nav-tabs" role="tablist">
+              <li class="active"><a href="#recent" role="tab" data-toggle="tab">recent</a></li>
+              <li><a href="#popular" role="tab" data-toggle="tab">popular</a></li>
+              <li><a href="#random" role="tab" data-toggle="tab">random</a></li>
+              <li><a href="#all" role="tab" data-toggle="tab">all</a></li>
             </ul>
+            
+            <!-- Tab panes -->
+            <div class="tab-content">
+              <div class="tab-pane active" id="recent">
+                <div class = "content-default containter-fluid">
+                <article class = "media">
+                  <img class = "media-object pull-left" src = "../images/287.jpg" alt = "a picture of a kitten">
+                  <div class = "media-body">
+                    <h5 class = "route-name">Route Name</h5>
+                    <h6 class = "route-location"><small>City Name, ST | created 09.04.2014</small></h6>
+                    <h4>difficulty:</h4>
+                    <h4 class = "rating-avg">rating: </h4>
+                    <p class = "voteNum"></p>
+                  </div>
+                </article>
+                <hr>
+                <article class = "media">
+                  <img class = "media-object pull-left" src = "../images/287.jpg" alt = "a picture of a kitten">
+                  <div class = "media-body">
+                    <h5 class = "route-name">Route Name</h5>
+                    <h6 class = "route-location"><small>City Name, ST | created 09.04.2014</small></h6>
+                    <h4>difficulty:</h4>
+                    <h4 class = "rating-avg">rating: </h4>
+                    <p class = "voteNum"></p>
+                  </div>
+                </article>
+                <hr>
+                <article class = "media">
+                  <img class = "media-object pull-left" src = "../images/287.jpg" alt = "a picture of a kitten">
+                  <div class = "media-body">
+                    <h5 class = "route-name">Route Name</h5>
+                    <h6 class = "route-location"><small>City Name, ST | created 09.04.2014</small></h6>
+                    <h4>difficulty:</h4>
+                    <h4 class = "rating-avg">rating: </h4>
+                    <p class = "voteNum"></p>
+                  </div>
+                </article>
+                 <ul class="pagination">
+                    <li><a href="#">&laquo;</a></li>
+                    <li><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li><a href="#">&raquo;</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="tab-pane" id="popular">
+                <div class = "content-default containter-fluid">
+                <article class = "media">
+                  <img class = "media-object pull-left" src = "../images/287.jpg" alt = "a picture of a kitten">
+                  <div class = "media-body">
+                    <h5 class = "route-name">Route Name</h5>
+                    <h6 class = "route-location"><small>City Name, ST | created 09.04.2014</small></h6>
+                    <h4>difficulty:</h4>
+                    <h4 class = "rating-avg">rating: </h4>
+                    <p class = "voteNum"></p>
+                  </div>
+                </article>
+                <hr>
+                <article class = "media">
+                  <img class = "media-object pull-left" src = "../images/287.jpg" alt = "a picture of a kitten">
+                  <div class = "media-body">
+                    <h5 class = "route-name">Route Name</h5>
+                    <h6 class = "route-location"><small>City Name, ST | created 09.04.2014</small></h6>
+                    <h4>difficulty:</h4>
+                    <h4 class = "rating-avg">rating: </h4>
+                    <p class = "voteNum"></p>
+                  </div>
+                </article>
+                <hr>
+                <article class = "media">
+                  <img class = "media-object pull-left" src = "../images/287.jpg" alt = "a picture of a kitten">
+                  <div class = "media-body">
+                    <h5 class = "route-name">Route Name</h5>
+                    <h6 class = "route-location"><small>City Name, ST | created 09.04.2014</small></h6>
+                    <h4>difficulty:</h4>
+                    <h4 class = "rating-avg">rating: </h4>
+                    <p class = "voteNum"></p>
+                  </div>
+                </article>
+                 <ul class="pagination">
+                    <li><a href="#">&laquo;</a></li>
+                    <li><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li><a href="#">&raquo;</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="tab-pane" id="random">
+                <div class = "content-default containter-fluid">
+                <article class = "media">
+                  <img class = "media-object pull-left" src = "../images/287.jpg" alt = "a picture of a kitten">
+                  <div class = "media-body">
+                    <h5 class = "route-name">Route Name</h5>
+                    <h6 class = "route-location"><small>City Name, ST | created 09.04.2014</small></h6>
+                    <h4>difficulty:</h4>
+                    <h4 class = "rating-avg">rating: </h4>
+                    <p class = "voteNum"></p>
+                  </div>
+                </article>
+                <hr>
+                <article class = "media">
+                  <img class = "media-object pull-left" src = "../images/287.jpg" alt = "a picture of a kitten">
+                  <div class = "media-body">
+                    <h5 class = "route-name">Route Name</h5>
+                    <h6 class = "route-location"><small>City Name, ST | created 09.04.2014</small></h6>
+                    <h4>difficulty:</h4>
+                    <h4 class = "rating-avg">rating: </h4>
+                    <p class = "voteNum"></p>
+                  </div>
+                </article>
+                <hr>
+                <article class = "media">
+                  <img class = "media-object pull-left" src = "../images/287.jpg" alt = "a picture of a kitten">
+                  <div class = "media-body">
+                    <h5 class = "route-name">Route Name</h5>
+                    <h6 class = "route-location"><small>City Name, ST | created 09.04.2014</small></h6>
+                    <h4>difficulty:</h4>
+                    <h4 class = "rating-avg">rating: </h4>
+                    <p class = "voteNum"></p>
+                  </div>
+                </article>
+                 <ul class="pagination">
+                    <li><a href="#">&laquo;</a></li>
+                    <li><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li><a href="#">&raquo;</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="tab-pane" id="all">
+                <div class = "content-default containter-fluid">
+                <article class = "media">
+                  <img class = "media-object pull-left" src = "../images/287.jpg" alt = "a picture of a kitten">
+                  <div class = "media-body">
+                    <h5 class = "route-name">Route Name</h5>
+                    <h6 class = "route-location"><small>City Name, ST | created 09.04.2014</small></h6>
+                    <h4>difficulty:</h4>
+                    <h4 class = "rating-avg">rating: </h4>
+                    <p class = "voteNum"></p>
+                  </div>
+                </article>
+                <hr>
+                <article class = "media">
+                  <img class = "media-object pull-left" src = "../images/287.jpg" alt = "a picture of a kitten">
+                  <div class = "media-body">
+                    <h5 class = "route-name">Route Name</h5>
+                    <h6 class = "route-location"><small>City Name, ST | created 09.04.2014</small></h6>
+                    <h4>difficulty:</h4>
+                    <h4 class = "rating-avg">rating: </h4>
+                    <p class = "voteNum"></p>
+                  </div>
+                </article>
+                <hr>
+                <article class = "media">
+                  <img class = "media-object pull-left" src = "../images/287.jpg" alt = "a picture of a kitten">
+                  <div class = "media-body">
+                    <h5 class = "route-name">Route Name</h5>
+                    <h6 class = "route-location"><small>City Name, ST | created 09.04.2014</small></h6>
+                    <h4>difficulty:</h4>
+                    <h4 class = "rating-avg">rating: </h4>
+                    <p class = "voteNum"></p>
+                  </div>
+                </article>
+                 <ul class="pagination">
+                    <li><a href="#">&laquo;</a></li>
+                    <li><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li><a href="#">&raquo;</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div> 
           </nav>
-          <article class = "row">
-            <img class = "media-object" src = "../images/287.jpg" alt = "a picture of a kitten">
-            <h5 class = "route-name">Route Name</h5>
-            <h6 class = "route-location"><small>City Name, ST | created 09.04.2014</small></h6>
-            <h4>difficulty: <a class = "dif-lo">lo</a> | <a class = "dif-med">med</a> | <a class = "dif-hi">hi</a></h4>
-            <h4 class = "rating-avg">rating: </h4>
-            <p class = "voteNum"></p>
-          </article>
-          <article class = "row">
-            <img class = "media-object" src = "../images/287.jpg" alt = "a picture of a kitten">
-            <h5 class = "route-name">Route Name</h5>
-            <h6 class = "route-location"><small>City Name, ST | created 09.04.2014</small></h6>
-            <h4>difficulty: <a class = "dif-lo">lo</a> | <a class = "dif-med">med</a> | <a class = "dif-hi">hi</a></h4>
-            <h4 class = "rating-avg">rating: </h4>
-            <p class = "voteNum"></p>
-          </article>
-          <article class = "row">
-            <img class = "media-object" src = "../images/287.jpg" alt = "a picture of a kitten">
-            <h5 class = "route-name">Route Name</h5>
-            <h6 class = "route-location"><small>City Name, ST | created 09.04.2014</small></h6>
-            <h4>difficulty: <a class = "dif-lo">lo</a> | <a class = "dif-med">med</a> | <a class = "dif-hi">hi</a></h4>
-            <h4 class = "rating-avg">rating: </h4>
-            <p class = "voteNum"></p>
-          </article>
         </section>
-        <aside class = "col-lg-3">
+        <aside class = "col-lg-3 col-md-3 col-sm-6">
         <?php require_once('../php/sidebar_routes.php');?>
         </aside>
     </div>
