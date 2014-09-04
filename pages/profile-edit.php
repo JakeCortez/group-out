2903029d646151f31bb964e8122cad967e3b3265
Kazim Agirnas <aagirnas@gmail.com>
2014-09-04 17:33:02 -0600

    added profile-edit content

--- pages/profile-edit.php
+++ pages/profile-edit.php
@@ -7,19 +7,29 @@
     <?php require_once('../php/nav.html');?>
     <div class = "container-fluid">
       <article class = "col-lg-9">
-       <form id="signUpForm" method="post" action="jquery-form.php">
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" placeholder="john_smith@email.com"/><br />
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password" placeholder="abc123" /><br />
-        <label for="confirmPassword">Confirm Password</label>
-        <input type="password" id="confirmPassword" name="confirmPassword" placeholder="abc123" /> <br />
-        <label for="member">Member Number</label>
-        <input type="number" id="member" name="member" placeholder="abc123"/><br />
-        <label type="napHours">Nap Hours</label>
-        <input type="number" min="0" step="1.75" id="napHours" name="napHours" placeholder="Sleep Well!" /> <br />
-        <button type="submit">Nap Time!</button>
-    </form>   
+        <img src="url" alt="Profile Picture"><a href="">Upload Profile Image</a>
+        <form id="displayName" method="post" action="">
+            <label for="displayName"><h4>Display Name</h4></label><p> Choose a name you would like to be visible to public</p><br />
+            <h4>Preferred Activities</h4><p> Choose one or more</p>
+            <input name="activity" type="checkbox" value="hike">Hike<input type="checkbox" value="bike">Bike<input type="checkbox" value="run">Run<input type="checkbox" value="other">Other<br /><br />
+            
+            
+            <label for="city"><h4>City</h4></label>
+            <input type="text" id="city" name="city" placeholder="abc123"/><br />
+            <label for="state"><h4>State</h4></label><input type="text" id="state" name="state" placeholder="abc123"/><label for="zip">
+            <h4>Zip Code</h4></label><input type="text" id="zipCode" name="zipCode" placeholder="12345" minlength="5" maxlength="5">
+            <label for="aboutMe"><h4>Describe Yourself</h4></label>
+            <input type="text" max="300" id="aboutMe" name="aboutMe" placeholder="abc123" /> <br />
+            <label for="website"><h4>Website (optional)</h4<label><br />
+            <input type="url" id="website" name="website" placeholder="abc@groupout.com"/>
+            <label for="faceboook"><h4>Facebook (optional)</h4<label><br />
+            <input type="text" id="facebook" name="facebook" placeholder="abc@groupout.com"/>
+            <label for="twitter"><h4>Twitter (optional)</h4<label><br />
+            <input type="text" id="twitter" name="twitter" placeholder="abc123"/>
+            <label for="instagram"><h4>Instagram (optional)</h4<label><br />
+            <input type="text" id="instagram" name="instagram" placeholder="abc123"/>
+            
+        </form>
       </article>
     
       <aside class = "col-lg-3">
