cfbda9dfee9ff217f9e5bdb14bbdc0ad727ff85e
Jim wittwer <wittwerjim@gmail.com>
2014-09-05 12:29:27 -0600

    updating my-profile html

--- profile/my-profile.php
+++ profile/my-profile.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 
-<?php require_once('/php/head.html');?>
+<?php require_once('../php/head-jim.html');?>
  <body>
 
    <?php require_once('../php/nav.html');?>
@@ -49,6 +49,6 @@
        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
    <!-- Include all compiled plugins (below), or include individual files as needed -->
-   <script src="../bootstrap/js/bootstrap.min.js"></script>
+   <script src="js/bootstrap.min.js"></script>
  </body>
 </html>
