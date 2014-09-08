6072299728cb6149a844e0c30a9f9e563fc87d1c
jLieberman <jlieberman1@cnm.edu>
2014-09-08 09:38:56 -0600

    make validate file universal

--- js/validator.js
+++ js/validator.js
@@ -1,14 +1,11 @@
 $(document).ready(
     function () {
      $("#eventName").validate( {
-        //DEBUG MODE: stop the form from being submitted
-        // care to remove after debugging!!!!
         //debug: true,
-        //debug is best to check if rules worked...
         // error class: formatting of messages
         errorClass: "badForm",
         
-        //rules to define how fields should be sanitized
+        //How to sanitize fields 
         rules: {
             eventName: {
                 required: true
@@ -20,7 +17,7 @@ $(document).ready(
                 required: true
             },
             eventZipCode: {
-                size: 5,
+                size: 10,
                 required: true
             },
             eventDescription: {
@@ -41,8 +38,8 @@ $(document).ready(
                 required: "Please submit the two-letter code of the state where the event will be held",
             },
             eventZipCode: {
-                size: "Zip Code must be five numbers long",
-                required: "Please add zip code!"                
+                size: "Please add your zip code",
+                required: "Zip Code Please!"                
             },
             eventDate: {
                 required:  "Please let us know when your event is being held!"
@@ -55,18 +52,11 @@ var zipTest = \d{5}(-\d{4}?);
    } else {
    }
              //set up AJAX call -- DOESN;T WORK IF DEBUG IS ON!
-            // submit Handler customizes form's -- no default behavior
              submitHandler: function(form) {
                 $(form).ajaxSubmit({
-                    //how do we send this data?  GET or POST?
                     type: "POST",
                     //url to submit to:
                     // .txt is a placeholder -- looking for PHP
-                    // SECURITY WARNING:  browsers don't allow you to
-                    //submit to an external server (to avoid cross-server
-                    //attacks) you can only load from your own.  PHP
-                    // will provide a safe way to get data from external
-                    // servers...  next week.
                     url: "jquery-form.php",
                     //data to be submitted
                     data: $(form).serialize(),
