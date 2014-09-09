0d9bf0b3e9572d99e0fdb78fefae60a05ebc57e7
jLieberman <jlieberman1@cnm.edu>
2014-09-09 14:05:35 -0600

    place head inside all html/php files

--- profile/my-profile.php
+++ profile/my-profile.php
@@ -1,7 +1,24 @@
 <!DOCTYPE html>
 <html>
-
-<?php require_once('../php/head.html');?>
+  <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <title>Group Out</title>
+          <!--custom css and javascript-->
+      <link href="../css/style.css" rel="stylesheet">
+            <!-- Bootstrap -->
+      <link href="../css/bootstrap.min.css" rel="stylesheet">
+      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+      <script src="../js/bootstrap.min.js"></script>
+          <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+          <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+          <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+          <![endif]-->
+      <script src="../js/group-out.js"></script>
+  </head>
  <body>
 
    <?php require_once('../php/nav.html');?>
