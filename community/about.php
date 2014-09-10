878351041508ea3ef7e1bf990f04b745bd19ac4d
Max DeHerrera <maxdehe3@gmail.com>
2014-09-10 12:12:22 -0600

    added content to aboutUs

--- community/about.php
+++ community/about.php
@@ -25,8 +25,11 @@
     <div class = "container-fluid">
       <article class = "col-lg-9">
         <h1>AboutUs</h1>
+        <div class="colorBlock" style="float:left;width:200px;height:200px;background-color:#c9f9a5;margin-right:20px;margin-bottom:20px;"></div>
+        <div style="clear: both;"></div>
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
       </article>
-    
+      
       <aside class = "col-lg-3">
       <?php require_once('../php/sidebarplaceholder.html');?>
       </aside>
