2ebf536634be88c8e3c887d94c377e4fe2e95c28
Jim wittwer <wittwerjim@gmail.com>
2014-09-10 10:11:59 -0600

    fixed head tag links

--- index.php
+++ index.php
@@ -6,26 +6,26 @@
      <meta name="viewport" content="width=device-width, initial-scale=1">
      <title>Group Out</title>
           <!--custom css and javascript-->
-      <link href="../css/style.css" rel="stylesheet">
+      <link href="css/style.css" rel="stylesheet">
             <!-- Bootstrap -->
-      <link href="../css/bootstrap.min.css" rel="stylesheet">
+      <link href="css/bootstrap.min.css" rel="stylesheet">
       <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-      <script src="../js/bootstrap.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
           <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
           <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
           <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
           <![endif]-->
-      <script src="../js/group-out.js"></script>
+      <script src="js/group-out.js"></script>
   </head>
   <body>
     <div class="page-header">
       <h1>group<strong>out</strong><small> lorem ipsum</small></h1>
     </div>
-    
+
     <?php require_once('php/nav.html');?>
-    
+
      <div class = "container-fluid">
       <div class = "row">
         <article class = "col-lg-12">
@@ -37,7 +37,7 @@
               <li data-target="images/meet_people.jpg" data-slide-to="1"></li>
               <li data-target="images/make_events.jpg" data-slide-to="2"></li>
             </ol>
-          
+
             <!-- Wrapper for slides -->
             <div class="carousel-inner">
               <div class="item active">
@@ -81,7 +81,7 @@
         </div>
       </div>
     </div>
-    
+
     <?php require_once('php/footer.html');?>
   </body>
 </html>
