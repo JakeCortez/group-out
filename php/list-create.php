a885ad927745adcdf9fac09237ebcecdb1508071
Jim wittwer <wittwerjim@gmail.com>
2014-09-26 11:10:32 -0600

    updated oo code in this file and link to pointer for mySQL connection

--- php/list-create.php
+++ php/list-create.php
@@ -1,44 +1,26 @@
 <?php
+include "../../config/Pointer.php";
+include "event-class.php";
 
 try {
-  // throw exceptions instead of PHP errors
-  mysqli_report(MYSQLI_REPORT_STRICT);
+  // call the Pointer static method to connect to mySQL
+  $mysqli = Pointer::getPointer();
 
-  // connect!
-  $mysqli = new mysqli("localhost", "root", "", "group_out");
-
-  // create & prepare a query template
-  $query = "SELECT eventCity, eventDate, eventDifficulty, eventMemberCount, eventName, eventState FROM Events WHERE creatorUserID = ? AND eventPrivacy = 2 LIMIT 3";
-
-  // prepare the statement
-  if(($statement = $mysqli->prepare($query)) === false) {
-    throw(new mysqli_sql_exception("Unable to prepare query $query"));
-  }
-
-  // bind parameters to the template
-  $userID = 1; // yucky hard wired value - get this from a session...
-  if(($statement->bind_param("i", $userID)) === false) {
-    throw(new mysqli_sql_exception("Unable to bind parameters"));
-  }
-
-  // execute the query
-  if(($statement->execute()) === false) {
-    throw(new mysqli_sql_exception("Unable to execute statement"));
-  }
-
-  // be demanding and get results! *pounds fist*
-  if(($result = $statement->get_result()) === false) {
-    throw(new mysqli_sql_exception("Unable to get results"));
-  }
-
+  // call the class static method for querying/getting 3 event results
+  $eventArray = Event::getEventsByCreatorUserID($mysqli, 1);
 
   // loop through the result set
-  while(($row = $result->fetch_assoc()) !== null) {
+  foreach($eventArray as $event) {
 
 
     // reformat the date
-    $dateTime = DateTime::createFromFormat("Y-m-d H:i:s", $row["eventDate"]);
+    $dateTime = $event->getEventDate();
     $niceDate = $dateTime->format("F j, Y");
+    $eventName = $event->getEventName();
+    $eventCity = $event->getEventCity();
+    $eventState = $event->getEventState();
+    $eventDifficulty = $event->getEventDifficulty();
+    $eventMemberCount = $event->getEventMemberCount();
 
     // echo the result
     echo <<<EOD
@@ -46,14 +28,14 @@ try {
       <div class="listThumb"></div>
 
       <div class="listDetails">
-        <div class="listHead">$row[eventName] | bike</div>
-        <div class="listInfo">$row[eventCity], $row[eventState] | $niceDate</div>
-        <div class="listDifficulty">difficulty / $row[eventDifficulty]</div>
+        <div class="listHead">$eventName | bike</div>
+        <div class="listInfo">$eventCity, $eventState | $niceDate</div>
+        <div class="listDifficulty">difficulty / $eventDifficulty</div>
       </div>
 
       <div class="listJoin">
         <div class="numberJoined">
-          <p class="number">$row[eventMemberCount]</p>
+          <p class="number">$eventMemberCount</p>
           <p>joined</p>
         </div>
       </div>
@@ -63,24 +45,8 @@ try {
       </div>
 EOD;
   }
-
-  // clean up the result set
-  $result->free();
-
-  // free up the statement
-  $statement->close();
-
-  // now, unplug the mySQL connection
-  $mysqli->close();
 } catch(mysqli_sql_exception $error) {
-  // echo $error
+  echo 'oops';
 }
 
-// THINGS THAT STILL NEED TO HAPPEN
-// the active user needs to be id'd first, so that the list data is related to them
-// for the profile page, the event, route and group lists need to be related to the active user
-// the join button needs to trigger that the active user has joined the event
-  // if the active user already joined an event, the button should say "joined"
-// eventActivity is actually going to be a separate table
-// eventJoined is going to see who's joined the event, but people can unjoin them too
 ?>
