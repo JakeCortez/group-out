9aa69b26fb0fb3df48c749487cf13ee3346d1ac8
jLieberman <jlieberman1@cnm.edu>
2014-09-30 17:21:20 -0600

    initializing user -- in progress

---
+++ php/logIn.php
@@ -0,0 +1,6 @@
+<?php
+$db_hostname = "localhost";
+$db_database = "group-out";
+$db_userID   = "user";
+$db_password = "password";
+?>
--- php/registration.php
+++ php/registration.php
@@ -7,13 +7,25 @@
             </title>
        
         <?php
-            $email = $_POST["email"];
-            $password = $_POST["password"];
-            $confirmPassword = $_POST["confirmPassword"]; ?>
+            $email = mysql_fix_string($_POST["email"]);
+            $password = mysql_fix_string($_POST["password"]);
+            $confirmPassword = mysql_fix_string($_POST["confirmPassword"]);
+            
+            ?>
         </head>
         <body>
         
-        <p> Welcome to Group-Out! <?php echo $email; ?> 
+        <p> Welcome to Group-Out! <?php echo $email; ?>
+        
+        <?php
+        
+            $newUser = new User ("email", "password");
+            $newUser->($mysqli);
+        
+        ?>
+        
+        
+        
             
         </form>
         </body>
