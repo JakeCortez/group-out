49c136e07ff0757324f0349099b5b25870b426d5
jLieberman <jlieberman1@cnm.edu>
2014-09-30 15:57:10 -0600

    building bridge between user class and DB

--- php/registration.php
+++ php/registration.php
@@ -1,16 +1,16 @@
-<!DOCTYPE html>
+
+    <!DOCTYPE html>
     <html>
         <head>
             <title>
                 Form registers input from new user
             </title>
-        </head>
+       
         <?php
             $email = $_POST["email"];
             $password = $_POST["password"];
-            $confirmPassword = $_POST["confirmPassword"];
-        
-        ?>
+            $confirmPassword = $_POST["confirmPassword"]; ?>
+        </head>
         <body>
         
         <p> Welcome to Group-Out! <?php echo $email; ?> 
