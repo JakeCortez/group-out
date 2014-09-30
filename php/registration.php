230b04513cb23f24d0cc85ba2aa7c40f49fdfc8b
jLieberman <jlieberman1@cnm.edu>
2014-09-30 15:21:35 -0600

    instantiate user in DB

--- php/registration.php
+++ php/registration.php
@@ -13,24 +13,8 @@
         ?>
         <body>
         
-        <p> Welcome to Group-Out! <?php echo $email; ?> <br />
-        We would like to know a little about you for our files.  <br />
-        If you would be so kind as to tell us
-        
-            <label for="firstName">First Name</label>
-            <input type="text" id="userFirstName" name="firstName" /><br />
-            <label for="lastName">Last Name</label>
-            <input type="text" id="userLastName" name="lastName" /><br />
-            <label for="avatar">Avatar File (JPG and PNG only )</label>
-            <input type="file" id="avatar" name="avatar" /><br />
-            <button class = "btn btn-default" type="submit">Save My Profile</button>
-          
-        
-        
-        </p>     
-      <
-       
-        <p> Your message is:
-        <?php //echo $_POST["message"];  ?></p>       
+        <p> Welcome to Group-Out! <?php echo $email; ?> 
+            
+        </form>
         </body>
     </html>
