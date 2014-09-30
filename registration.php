bdbd39b39afacaddfdc53485a160c2dbfc30bbb4
jLieberman <jlieberman1@cnm.edu>
2014-09-30 11:31:54 -0600

    cleaning up registration form

---
+++ registration.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>
+                Form registers new user
+            </title>
+        </head>
+        <body>
+            <form method = "post" action = "send_RegistrationForm.php"
+        
+        
+        
+        
+        
+        </body>
+    </html>
