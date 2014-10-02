b787acd583e6d8ef71f10bf81249f2fd7fca46fa
Jake Cortez <jcortez96@hotmail.com>
2014-10-02 17:11:17 -0600

    added confirmation message and added comments

--- php/group-processor.php
+++ php/group-processor.php
@@ -4,6 +4,7 @@
     
     $_SESSION["userID"] = 2;
     
+    //check if userID exists
     if(notset($_SESSION["userID"])){
         echo("You must be logged into an account to create a group");
     }
@@ -13,13 +14,15 @@
     require_once("../classes/user-login.php");
     require_once("../classes/group-class.php");
     
-    //set up connection to server and get user info
+    //set up connection to server
     try{
         $mysqli = Pointer::getPointer();
     }
     catch(mysqli_sql_excpetion $error){
         throw(new mysqli_sql_exception("could not connect to server", 0, $error));
     }
+    
+    //create group object
     try{
     $group  = Group( null, $_SESSION["userID"], null, $_POST["groupAvatar"], $_POST["groupCity"],
                      $_POST["groupDescription"], $_POST["groupName"], $_POST["groupSkill"],
@@ -31,10 +34,15 @@
     catch(RangeException $error){
         throw(new RangeException("sorry something went wrong when creating your group"));
     }
+    
+    //insert group into database
     try{
         $this->group->insert($this->mysqli);
     }
     catch(mysqli_sql_exception $error){
         throw(new mysqli_sql_exception("sorry, could not save group"));
     }
+    
+    //we did it!
+    echo("you succesfully created a group");
 ?>
