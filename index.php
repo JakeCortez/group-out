fe87df2f273360e3477038de1788ed8171828e4b
jLieberman <jlieberman1@cnm.edu>
2014-10-02 08:42:10 -0600

    get js/validator to reach registration modal

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
           <![endif]-->
-      <script src="js/validator.js"></script>
+
   </head>
   <body>
     <div class="page-header">
@@ -77,5 +77,10 @@
       </div>
     </div>
     <?php require_once('php/footer-index.html');?>
+            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+          <script src="../js/validator.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
   </body>
 </html>
