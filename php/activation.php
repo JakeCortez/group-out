fe4791587897a03b44044ca091d3018df6e6d1d5
jLieberman <jlieberman1@cnm.edu>
2014-10-03 11:11:59 -0600

    working on page to activate registered user

--- php/activation.php
+++ php/activation.php
@@ -2,3 +2,15 @@
 
 
 ?>
+
+
+// page to MATCH new (unapproved) USER AUTH TOKEN
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title></title>
+        </head>
+        <body>
+            
+        </body>
+    </html>
