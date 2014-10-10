234bffa439dae74411883353135d7d6ab5fd8067
Colton Trujillo <trujillo.cbt@gmail.com>
2014-10-10 09:49:46 -0600

    added updated mapinitreadfunction and route-save

--- routes/route-save.php
+++ routes/route-save.php
@@ -1,5 +1,20 @@
 <?
-function insert($query){
+//ob_start(); turns on output buffering meaning php output is stored locally until we decide to send it back to page
+header('Cache-Control: no-store, no-cache, must-revalidate');//sets catch-control on http header...
+require_once("/etc/apache2/capstone-mysql/group-out.php");
+
+try{
+    $mysqli = Pointer::getPointer();
+} catch (mysqli_sql_exception $sqlException) {
+    //handle connection error
+    throw(new mysqli_sql_exception("Unable to connect to the database"));
+}
+
+$data = $_REQUEST['mapdata'];//data is assigned to string we passed using saveMapFunction
+$routeData = $_REQUEST['routeData'];//routeData is assigned to data we passed from SaveMapFunction
+$routeData = json_decode($routeData);//turns the string into an object so we can grab the parts in our bind_param
+
+$query = "INSERT INTO routes(userID, routeName, routeDescription, routeDifficulty, routePrivacy, routeData) VALUES(?, ?, ?, ?, ?, ?)";
 $statement = $mysqli->prepare($query);
 
 if($statement === false){
@@ -15,23 +30,10 @@ if($clean === false){
 if($statement->execute() === false){
     throw(new mysqli_sql_exception("error"));
 }
-}
-
-ob_start(); // turns on output buffering meaning php output is stored locally until we decide to send it back to page
-header('Cache-Control: no-store, no-cache, must-revalidate');//sets catch-control on http header...
-require_once("/etc/apache2/capstone-mysql/group-out.php");
-$data = $_REQUEST['mapdata'];//data is assigned to string we passed using saveMapFunction
-$routeData = $_REQUEST['routeData'];//routeData is assigned to data we passed from SaveMapFunction
-$routeData = json_decode($routeData);//turns the string into an object so we can grab the parts in our bind_param
-try{
-    $mysqli = Pointer::getPointer();
-} catch (mysqli_sql_exception $sqlException) {
-    //handle connection error
-    throw(new mysqli_sql_exception("Unable to connect to the database"));
-}
 
 
-$query = "INSERT INTO routes(userID, routeName, routeDescription, routeDifficulty, routePrivacy, routeData) VALUES(?, ?, ?, ?, ?, ?)"; 
+
+
 insert($query);
 
 ?>
